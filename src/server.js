2c9e0b0f40197ad92ed8ad3e42442ebc0812bf98
goleacornelia <goleacornelia725@gmail.com>
2023-10-09 15:23:53 +0100

    update

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 const express = require("express");
 
 const app = express();
+// http://localhost:5001/
+// app.use("/example", express.static("example"));
+
+app.use("/", express.static("home"));
+
+app.use("/about", express.static("about"));
+
+app.use("/contact", express.static("contact"));
 
 app.listen(5001, () => {
   console.log("Server is listening");
