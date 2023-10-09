43c2b3cead3c0f4da537da973da38a1588f9d1d0
goleacornelia <goleacornelia725@gmail.com>
2023-10-09 17:31:06 +0100

    enchancement: add routes postget server.js

--- src/server.js
+++ src/server.js
@@ -2,7 +2,43 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/example", express.static("example"));
+app.use(express.json());
+
+app.get("/books", (request, response) => {
+  console.log(request.originalUrl);
+  const book = {
+    title: "lord of the rings",
+    author: "tolkein",
+    genre: "fantasy",
+  };
+
+  const successResponse = {
+    message: "success",
+    book: book,
+  };
+
+  response.send(successResponse);
+});
+
+app.post("/books", (request, response) => {
+  console.log(request.body.title);
+  const newBook = {
+    title: request.body.title,
+    author: request.body.author,
+    genre: request.body.genre,
+  };
+  console.log("hello world");
+
+  // create a book on the de(
+  //     title: request.body.title,
+  // )
+  const successResponse = {
+    message: "success",
+    newBook: newBook,
+  };
+
+  response.send(successResponse);
+});
 
 app.listen(5001, () => {
   console.log("Server is listening");
