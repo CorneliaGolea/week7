cc92fee6dba580a07a64f90b3709152569b2f4bd
goleacornelia <goleacornelia725@gmail.com>
2023-10-12 15:50:19 +0100

    stretch goals

--- src/books/routes.js
+++ src/books/routes.js
@@ -7,32 +7,25 @@ const {
   findAllBooks,
   createBook,
   deleteBook,
-  findOneAndUpdate,
+  updateAuthor,
+
+  updateByTitleDynamic,
+  // deleteAllBooks,
+  deleteOneOrAll,
+  findOne,
 } = require("./controllers");
 
 bookRouter.get("/books", findAllBooks);
 bookRouter.delete("/books", deleteBook);
 bookRouter.post("/books", createBook);
-bookRouter.put("/books", findOneAndUpdate);
+bookRouter.put("/books", updateAuthor);
 
-// app.put("/books/dynamic", async (request, response) => {
-//   console.log(request.originalUrl);
-//   const findOneAndUpdate = await Book.findOneAndUpdate(
-//     { title: request.body.title },
-//     { author:  },
-//     { returnDocument: "after" }
-//   );
-
-//   const successResponse = {
-//     message: "success",
-//     patchBook: patchBook,
-//   };
-
-//   response.send(successResponse);
-// });
-
+bookRouter.put("/books/dynamic", updateByTitleDynamic);
+// bookRouter.delete("/books/deleteAll", deleteAllBooks);
+bookRouter.delete("/books/deleteOneOrAll", deleteOneOrAll);
+bookRouter.get("/books/:title", findOne);
+/////////////////////////////
 // console.log(request.body.title);
-// const newBook = {
 //   title: request.body.title,
 //   author: request.body.author,
 //   genre: request.body.genre,
